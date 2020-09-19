2e252ccaacf61adc66cefbc3572074f5c7b9dcb6
Denis <kfdfk123456789@gmail.com>
2020-09-20 00:17:57 +0500

    Save

--- user.php
+++ user.php
@@ -2,13 +2,9 @@
 //isset($_GET['first_name']);
 //$first_name = $_POST['first_name'];
 //echo json_encode($first_name);
-if (isset($_GET['first_name'])) {//Надо доделать
-
+if (isset($_POST['first_name'])) {
     $first_name = $_POST['first_name'];
-
-    $query = $pdo -> query("SELECT * FROM users");
-    $user = $query -> fetch(PDO::FETCH_ASSOC);
+    $query = $pdo->query("SELECT * FROM users WHERE first_name = '{$first_name}'");
+    $user = $query->fetch(PDO::FETCH_ASSOC);
     echo json_encode(array($user));
-
 }
-
