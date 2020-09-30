e1e34e86ab9ae56977494d33079ed5cf29043172
Denis <kfdfk123456789@gmail.com>
2020-09-30 12:54:47 +0500

    Save

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
 jQuery(document).ready(function ($) {
-    console.log("Hello World");
+    var appToken = null;
 
+    console.log("init");
+
+    $("#login").addClass('_inline-block');
+
+    $("#link_reg").click(function (e){
+        e.preventDefault();
+
+        $("#login").removeClass('_inline-block');
+        $("#reg").addClass('_inline-block');
+    });
+
+    $("#link_log").click(function (e){
+        e.preventDefault();
+
+        $("#reg").removeClass('_inline-block');
+        $("#login").addClass('_inline-block');
+    });
+
+    $("#button_log").click(function (e){
+        e.preventDefault();
+
+        var phone = $("input[name ='login_phone']").val();
+        var password = $("input[name ='login_password']").val();
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/login',
+            data: {
+              phone: phone,
+              password: password
+            },
+            success: function(data){
+                appToken = data.token;
+                console.log(appToken);
+            }
+        })
+    });
 
 
 });
-console.log("HelloWorld");
