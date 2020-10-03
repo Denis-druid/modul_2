36f2d8c7a161cf7c2587cbb73a3351420adcb02a
Denis <kfdfk123456789@gmail.com>
2020-10-03 22:20:07 +0500

    save

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-// jQuery(document).ready(function ($) {
-//     var appToken = null;
-//
-//     console.log("init");
-//
-//     $("#login").addClass('_inline-block');
-//
-//     $("#link_reg").click(function (e){
-//         e.preventDefault();
-//         //
-//         // $("#login").removeClass('_inline-block');
-//         // $("#reg").addClass('_inline-block');
-//     });
-//
-//     $("#link_log").click(function (e){
-//         e.preventDefault();
-//
-//         // $("#reg").removeClass('_inline-block');
-//         // // $("#login").addClass('_inline-block');
-//
-//     });
-//
-//     $("#button_log").click(function (e){
-//         e.preventDefault();
-//         console.log("helkfngh;oeruydf")
-//
-//         })
-//
-//
-//
-// });
+jQuery(document).ready(function ($) {
+    var appToken = null;
+
+    console.log("init");
+
+    $("#login").addClass('_inline-block');
+
+    $("#link_reg").click(function (e){
+        e.preventDefault();
+        //
+        // $("#login").removeClass('_inline-block');
+        // $("#reg").addClass('_inline-block');
+    });
+
+    $("#link_log").click(function (e){
+        e.preventDefault();
+
+        // $("#reg").removeClass('_inline-block');
+        // // $("#login").addClass('_inline-block');
+
+    });
+
+    $("#button_log").click(function (e){
+        e.preventDefault();
+        console.log("helkfngh;oeruydf")
+
+        })
+
+
+
+});
