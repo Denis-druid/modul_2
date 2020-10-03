8f42bdbe7c5808b3357beacf9afb7918fa91f68b
Denis <kfdfk123456789@gmail.com>
2020-10-04 00:09:44 +0500

    save

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ jQuery(document).ready(function ($) {
         $("#reg").addClass('_inline-block');
     });
 
+    $("#link-logout").click(function (e){
+        e.preventDefault();
+
+        $("#page").removeClass('_inline-block');
+        $("#login").addClass('_inline-block');
+    });
+
     $("#link_log").click(function (e){
         e.preventDefault();
 
@@ -19,25 +26,69 @@ jQuery(document).ready(function ($) {
         $("#login").addClass('_inline-block');
     });
 
-    $("#button_log").click(function (e){
+    $("#button_log").click(function (e){//Вход в аккаунт
         e.preventDefault();
 
-        var phone = $("input[name ='login_phone']").val();
-        var password = $("input[name ='login_password']").val();
+        var phone = $("input[name='login-phone']").val();
+        var password = $("input[name='login-password']").val();
+
 
         $.ajax({
             type: 'POST',
-            url: '/api/login',
+            url: 'http://localhost/api/login',
             data: {
                 phone: phone,
                 password: password
             },
-            success: function(data){
+            success: function (data) {
                 appToken = data.token;
-                console.log(appToken);
+
+                if (appToken) {
+                    initUser();
+                }
             }
-        })
+        });
+
     });
+
+    function initUser() {
+
+        $("#login").removeClass('_inline-block');
+        $("#page").addClass('_inline-block');
+
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost/api/profile',
+            headers: {
+                'Authorization':'Bearer ' + appToken,
+            },
+            success: function (data) {
+                console.log(data.first_name + ' ' + data.surname);
+            }
+        });
+    };
+
+    $("#button_reg").click(function (e) {
+        e.preventDefault();
+
+        var first_name = $("input[name='reg-name']").val();
+        var surname = $("input[name='reg-surname']").val();
+        var phone = $("input[name='reg-phone']").val();
+        var password = $("input[name='reg-password']").val();
+
+        $.ajax({
+            type:'POST',
+            url:'http://localhost/api/signup',
+            data:{
+                first_name:first_name,
+                surname:surname,
+                phone:phone,
+                password:password
+            },
+        });
+
+    })
+
 
 
 });
--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
 
 if ($auth_user) {
 
-    $array = array('first-name' => $auth_user['first_name'],
+    $array = array('first_name' => $auth_user['first_name'],
                     'surname' => $auth_user['surname']);
     api_response($array);
 
